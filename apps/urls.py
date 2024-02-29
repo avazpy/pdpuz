1a0255d6a93903900538ce5e4ab232694144d094
Shawn1911 <azamovshahboz06082001@gmail.com>
2024-02-29 14:29:15 +0500

    update by shawn

--- apps/urls.py
+++ apps/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
