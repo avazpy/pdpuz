0fa18ef7e2f018825ceea1cbf7bd0432ca5a68c4
najmitdinovavaz <najmiddinovavaz2208@gmail.com>
2024-05-02 20:08:24 +0500

    unique qoyldi

--- apps/models.py
+++ apps/models.py
@@ -24,7 +24,7 @@ class User(AbstractUser):
                 regex=r'^\+?1?\d{9,13}$',
                 message="Phone number must be entered in the format '+998'. Up to 13 digits allowed."
             ),
-        ],
+        ], unique=True
     )
     balance = PositiveIntegerField(default=0)
     bot_options = CharField(max_length=255, null=True, blank=True)
@@ -84,7 +84,6 @@ class Lesson(CreatedBaseModel):
         ('inprog', 'INPROG'),
         ('finished', 'FINISHED'),
     ]
-
 
     order = IntegerField()
     status = CharField(choices=STATUS_CHOICES, default='BLOCKED')
