6217a8d00f32ce54a5d7995ffad6e16fcda1b7aa
najmitdinovavaz <najmiddinovavaz2208@gmail.com>
2024-06-01 18:23:46 +0500

    start_hendler.py change

--- tgbot/bot/heandler/start_hendler.py
+++ tgbot/bot/heandler/start_hendler.py
@@ -10,7 +10,7 @@ from tgbot.bot.loader import dp
 
 @dp.message(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer("nomeringizni kiriting", reply_markup=menu_buttons())
+    await message.answer("nomeringizni kiriting ☎", reply_markup=menu_buttons())
 
 
 @dp.message(F.content_type.in_({ContentType.CONTACT}))
@@ -31,4 +31,4 @@ async def phone_number_handler(msg: types.Message):
         await msg.answer(f"Your password is {password}")
         await msg.answer(f"link in sayt http://localhost:8005/")
     else:
-        await msg.answer("siz ro'yxatan o'tkansiz")
+        await msg.answer("siz ro'yxatan o'tkansiz 😊")
