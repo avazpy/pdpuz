ae20462b0fdb27e7256b01ad1447980e4544afad
izzatulloh2006 <zzatullohtoshqandi@gmail.com>
2024-06-22 14:53:17 +0500

    add code

--- tgbot/bot/handler/start_handler.py
+++ tgbot/bot/handler/start_handler.py
@@ -18,19 +18,21 @@ async def phone_number_handler(msg: types.Message):
     phone = msg.contact.phone_number
     password_in_database = make_password(f"{phone}{msg.from_user.id}")
     password = f"{phone}{msg.from_user.id}"
-    if not await User.objects.filter(phone_number=phone).aexists() and msg.from_user.username and not await User.objects.filter(
+    if not await User.objects.filter(
+            phone_number=phone).aexists() and msg.from_user.username and not await User.objects.filter(
             tg_id=msg.from_user.id).aexists():
         await User(last_name=msg.from_user.username, username=msg.from_user.username, phone_number=phone,
                    tg_id=msg.from_user.id, password=password_in_database).asave()
 
         await msg.answer(f"Your password is {password}")
-        await msg.answer(f"link in site http://localhost:8005/")
+        await msg.answer('link in site <a href="http://127.0.0.1:8000/">http://127.0.0.1:8000/</a>', parse_mode="HTML")
     elif not await User.objects.filter(phone_number=phone).aexists() and not await User.objects.filter(
             tg_id=msg.from_user.id).aexists():
         await User(last_name=str(msg.from_user.id), username=str(msg.from_user.id), phone_number=phone,
                    tg_id=msg.from_user.id, password=password_in_database).asave()
 
         await msg.answer(f"Your password is {password}")
-        await msg.answer(f"link in site http://localhost:8005/")
+        await msg.answer(f"link in site http://127.0.0.1:8000/")
     else:
-        await msg.answer("siz ro'yxatan o'tkansiz 😊")
+        await msg.answer("siz royxatdan o'tkansiz 😊")
+
